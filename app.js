6838734abf4354fadf65f2cdaeeeb62736fa7db1
Stanislav Ivanov <103877318+stanislaviw@users.noreply.github.com>
2022-07-06 14:00:18 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const button = document.getElementById('btn');
+const color = document.querySelector('.color')
+
+const hex = [ 
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+]
+
+button.addEventListener('click', () => {
+    let hexColor = gennerateHex();
+    document.body.style.backgroundColor = hexColor;
+    color.textContent = hexColor;
+});
+
+function gennerateHex() {
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()]
+    }
+
+    return hexColor;
+}
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
